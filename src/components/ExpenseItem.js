54858aef3c0942efad264bfd5927332cfaf8dff2
Matheus Andrade Fernal <matheusfernal@gmail.com>
2021-10-26 17:38:39 -0300

    Primeiro componente

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div>28 de março de 2021</div>
+      <div>
+        <h2>Seguro do caro</h2>
+        <div>R$ 698,67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
